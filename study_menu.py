f864b5d5338a43fbc2f9e757326e2921a4c78cea
BBYBOI <zeygermanleo@gmail.com>
2026-06-10 00:33:12 +0500

    add delete subject

--- study_menu.py
+++ study_menu.py
@@ -1,12 +1,13 @@
-from subject_logic import show_all, completed, special_show
+from subject_logic import show_all, completed, special_show, change_subject, delete_subject
 
 def study_menu():
     while True:
         try:
             print('1.Посмотреть предметы')
-            print('2.Отмеить выполнение')
+            print('2.Отметить выполнение')
             print('3.Изменить предмет')
-            print('4.Выход')
+            print('4.Удалить предмет')
+            print('5.Выход')
             choice = int(input('Ваш выбор: '))
             
             if choice == 1:
@@ -14,6 +15,15 @@ def study_menu():
                 
             if choice == 2:
                 completed()
+                
+            if choice == 3:
+                change_subject()
+            
+            if choice == 4:
+                delete_subject()
+                
+            if choice == 5:
+                exit()
             
         except ValueError:
             print('Ошибка при вводе!')
--- subject_logic.py
+++ subject_logic.py
@@ -48,3 +48,57 @@ def completed():
             item['complete'] = subject_object.complete
             save_data(data)
             subject_object.full_show()
+            
+            
+def change_subject():
+    data = load_data()
+    print('1.Изменить название предмета')
+    print('2.Изменить время изучения предмета')
+    print('3.Изменить цель предмета')
+    choice = int(input('Ваш выбор: '))
+    if choice == 1:
+        old_name = input('Введите старое название предмета: ')
+        word = None
+        for item in data:
+            word = item['name']
+            if old_name.lower() == word.lower():
+                new_name = input('Введите новое название: ')
+                item['name'] = new_name
+                save_data(data)
+                subject_object = Subject(item['name'], item['day_time'], item['target'], item['last_complete'], item['complete'])
+                subject_object.get_info()
+    if choice == 2:
+        name = input('Введите название предмета, у которого хотите изменить время: ')
+        word = None
+        for item in data:
+            word = item['name']
+            if name.lower() == word.lower():
+                new_time = int(input('Введите новое количество минут: '))
+                item['day_time'] = new_time
+                save_data(data)
+                subject_object = Subject(item['name'], item['day_time'], item['target'], item['last_complete'], item['complete'])
+                subject_object.get_info()
+                
+    if choice == 3:
+        name = input('Введите название предмета, у которого хотите изменить цель: ')
+        word = None
+        for item in data:
+            word = item['name']
+            if name.lower() == word.lower():
+                new_target = input('Введите новую цель: ')
+                item['target'] = new_target
+                save_data(data)
+                subject_object = Subject(item['name'], item['day_time'], item['target'], item['last_complete'], item['complete'])
+                subject_object.get_info()
+                
+                
+def delete_subject():
+    data = load_data()
+    name = input('Введите название предмета, который хотите удалить: ')
+    word = None
+    for item in data:
+        word = item['name']
+        if name.lower() == word.lower():
+            data.remove(item)
+            save_data(data)
+            print('Предмет успешно удален!')
